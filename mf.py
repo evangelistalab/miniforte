93c52f5bb9d1667a36efac4794b14e0f638d8edf
Francesco Evangelista <francesco.evangelista@gmail.com>
2020-06-17 15:49:36 -0400

    First commit

---
+++ mf.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+print(f"Welcome to miniforte")
+#    psi_options = psi4.core.get_options()
+#    psi_options.set_current_module('FORTE')
+#    forte_options.push_options_to_psi4(psi_options)
