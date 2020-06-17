1a21095a69c82aaf4ad71bb9acc475c162258bcb
Francesco Evangelista <francesco.evangelista@gmail.com>
2020-06-17 16:32:45 -0400

    Call options psi4 side

--- mf.py
+++ mf.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import psi4
 import miniforte
+
 print(f"Welcome to miniforte!")
 print(miniforte.add(3,4))
-#    psi_options = psi4.core.get_options()
-#    psi_options.set_current_module('FORTE')
+psi_options = psi4.core.get_options()
+psi_options.set_current_module('FORTE')
 #    forte_options.push_options_to_psi4(psi_options)
