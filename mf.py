9c7bf3432906675322cfb1db286da50a4431047d
Francesco Evangelista <francesco.evangelista@gmail.com>
2020-06-17 15:52:54 -0400

    Add printing

--- mf.py
+++ mf.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-print(f"Welcome to miniforte")
+import miniforte
+print(f"Welcome to miniforte!")
+print(miniforte.add(3,4))
 #    psi_options = psi4.core.get_options()
 #    psi_options.set_current_module('FORTE')
 #    forte_options.push_options_to_psi4(psi_options)
